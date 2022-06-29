765842b2d551cf36c85c97f0bb2dadfbb5ddc3e6
QibingLee <qblee.zju@gmail.com>
2022-06-29 22:01:45 +0800

    Delete dt-tracer directory

--- dt-tracer/src/antdv.js
+++
@@ -1,9 +0,0 @@
-import { Button, Input, Select, Modal, Spin } from 'ant-design-vue'
-
-export default (Vue) => {
-  Vue.use(Button)
-  Vue.use(Input)
-  Vue.use(Select)
-  Vue.use(Modal)
-  Vue.use(Spin)
-}
